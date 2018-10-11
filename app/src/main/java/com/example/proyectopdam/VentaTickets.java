d7086c5e1fe3a1538cf34db7a5ec46642a082aa1
Leonardo Herrera <leonardo.ft3@gmail.com>
2018-10-11 18:53:05 -0500

    Primeras ventanas creadas

---
+++ app/src/main/java/com/example/proyectopdam/VentaTickets.java
@@ -0,0 +1,14 @@
+package com.example.proyectopdam;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class VentaTickets extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venta_tickets);
+    }
+
+}
